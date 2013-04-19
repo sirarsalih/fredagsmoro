2dbc53f3e2c3997543bbd5a4132adcf216ff66de
Chris Searle <chris@chrissearle.org>
2013-04-19 13:08:08 +0200

    Initial

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source :rubygems
+
+gem 'nanoc'
+gem 'adsf'
+gem 'kramdown'
+gem 'rainpress'
+gem 'fssm'
+gem 'nutils'
+gem 'systemu'
+gem 'rubyzip'
---
+++ new_week.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+require 'zip/zip'
+
+output = <<-eot
+---
+title: #{ARGV[0]}
+fun: true
+---
+
+<h1>#{ARGV[0]}</h1>
+
+<% @item.children.each do |i| %>
+<p><img src="<%= i.path %>"/></p>
+<% end %>
+eot
+
+root = File.join(Dir::pwd, 'content', ARGV[0])
+
+FileUtils.mkdir_p(root)
+
+File.open("#{root}.erb", 'w') { |file| file.write(output) }
+
+Zip::ZipFile.open(ARGV[1]) do |zip_file|
+  zip_file.each do |file|
+    destination = File.join(root, file.name)
+    zip_file.extract(file, destination) unless File.exist?(destination)
+  end
+end
