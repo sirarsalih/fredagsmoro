531489b135afb4db6fb08ec378b11cfecdadf70c
Chris Searle <chris@chrissearle.org>
2014-09-05 17:39:26 +0200

    Remove nanoc build

--- lib/default.rb
+++
@@ -1,7 +0,0 @@
-# All files in the 'lib' directory will be loaded
-# before nanoc starts compiling.
-
-require "nutils"
-
-include Nanoc3::Helpers::Rendering
-include Nanoc3::Helpers::LinkTo
--- lib/helpers_.rb
+++
@@ -1,2 +0,0 @@
-include NextPrevHelper
-include MenuSplitHelper
