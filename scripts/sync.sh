c07c2d2a7fff1c56fa5619d4276edc552758f783
Chris Searle <chris@chrissearle.org>
2014-09-28 22:01:08 +0200

    Remove dup image. Update front page

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -rvz --delete --exclude=.git --exclude=.idea --exclude=.gitignore --exclude=.DS_Store --exclude=scripts ./ bryanek.chrissearle.org:/srv/www/fredagsmoro.chrissearle.org/htdocs
+rsync -rvz --delete --exclude=.git --exclude=.idea --exclude=.gitignore --exclude=.DS_Store --exclude=scripts ../ bryanek.chrissearle.org:/srv/www/fredagsmoro.chrissearle.org/htdocs
