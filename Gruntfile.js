7e8ea586ae3505073ddaf1a0a8a758da524505b2
Chris Searle <chris@chrissearle.org>
2014-11-15 14:28:00 +0100

    Initial gruntwork

---
+++ Gruntfile.js
@@ -0,0 +1,77 @@
+/* global module */
+
+module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        eslint: {
+            target: [
+                "Gruntfile.js",
+                "js/*.js",
+                "test/**/*Spec.js"
+            ]
+        },
+        "bower-install-simple": {
+            app: {
+                options: {
+                    color: true,
+                    production: false,
+                    cwd: ".",
+                    directory: "lib"
+                }
+            }
+        },
+        karma: {
+            app: {
+                configFile: "test/config/karma.conf.js"
+            },
+            "app-fast": {
+                browsers: ["PhantomJS"],
+                singleRun: true,
+                configFile: "test/config/karma.conf.js"
+            }
+        },
+        rsync: {
+            options: {
+                args: ["--verbose"],
+                exclude: [".git*", ".idea", "node_modules", "package.json", "*.log", ".DS_Store", "scripts", "build", "Gruntfile.js", "README.md", "bower.json", "test"],
+                recursive: true
+            },
+            build: {
+                options: {
+                    src: "./",
+                    dest: "./build",
+                    delete: true
+                }
+            },
+            prod: {
+                options: {
+                    src: "./build/",
+                    dest: "/srv/www/fredagsmoro.chrissearle.org/htdocs",
+                    host: "bryanek.chrissearle.org",
+                    delete: true
+                }
+            }
+        },
+        exec: {
+            data: {
+                cwd: "scripts",
+                command: "./build_data.rb > ../data.json"
+            }
+        }
+    });
+
+    // Package management
+    grunt.loadNpmTasks("grunt-npm-install");
+    grunt.loadNpmTasks("grunt-bower-install-simple");
+    grunt.loadNpmTasks("grunt-eslint");
+    grunt.loadNpmTasks("grunt-karma");
+    grunt.loadNpmTasks("grunt-rsync");
+    grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-git');
+
+    grunt.registerTask("install", ["bower-install-simple:app", "npm-install"]);
+    grunt.registerTask("default", ["eslint"]);
+    grunt.registerTask("newweek", ["exec:data"]);
+    grunt.registerTask("deploy", ["rsync:build", "rsync:prod"]);
+};
--- js/app.js
+++ js/app.js
@@ -1,145 +1,15 @@
-var app = angular.module('FDM', ['ngRoute' , 'ui.bootstrap']);
+/* global angular */
 
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'views/front.html'
-    }).when('/:year/:month/:day', {
-        templateUrl: 'views/show.html'
-    }).when('/slide/:year/:month/:day', {
-        templateUrl: 'views/carousel.html'
-    }).otherwise({redirectTo: '/'});
+var app = angular.module("FDM", ["ngRoute" , "ui.bootstrap"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    "use strict";
+    
+    $routeProvider.when("/", {
+        templateUrl: "views/front.html"
+    }).when("/:year/:month/:day", {
+        templateUrl: "views/show.html"
+    }).when("/slide/:year/:month/:day", {
+        templateUrl: "views/carousel.html"
+    }).otherwise({redirectTo: "/"});
 }]);
-
-app.factory('dataFactory', ['$http', function ($http) {
-    function getData(callback) {
-        $http({
-            method: 'GET',
-            url: 'data.json',
-            cache: true
-        }).success(callback);
-    }
-
-    return {
-        years: function (callback) {
-            getData(function (data) {
-                var years = [];
-
-                angular.forEach(data, function (year) {
-                    this.push({ 'name': year.name });
-                }, years);
-
-                callback(years);
-            });
-        },
-        monthsForYear: function (year, callback) {
-            getData(function (data) {
-                var months = [];
-
-                angular.forEach(data.filter(function (candidate) {
-                    return candidate.name == year;
-                })[0].tree, function (month) {
-                    var result = { 'name': month.name };
-
-                    var days = [];
-
-                    angular.forEach(month.tree, function (day) {
-                        this.push({ 'name': day.name });
-                    }, days);
-
-                    result['days'] = days;
-
-                    this.push(result);
-                }, months);
-
-                callback(months);
-            });
-        },
-        filesForYearMonthDay: function (year, month, day, callback) {
-            function filterTree(tree, match) {
-                return tree.filter(function (candidate) {
-                    return candidate.name == match;
-                })[0].tree;
-            }
-
-            getData(function (data) {
-                callback(filterTree(filterTree(filterTree(data, year), month), day));
-            });
-        },
-        latest: function (callback) {
-            var sortNameReverse = function (a, b) {
-                if (a.name < b.name) {
-                    return 1;
-                }
-                if (a.name > b.name) {
-                    return -1;
-                }
-                return 0;
-            };
-
-            getData(function (data) {
-                var year = data.sort(sortNameReverse)[0];
-
-                var month = year.tree.sort(sortNameReverse)[0];
-
-                var day = month.tree.sort(sortNameReverse)[0];
-
-                callback(year.name, month.name, day.name);
-            });
-        }
-    };
-}]);
-
-app.directive('calendar', ['dataFactory', function (dataFactory) {
-    return {
-        templateUrl: 'views/cal.html',
-        restrict: 'A',
-        scope: {
-            year: '='
-        },
-        link: function ($scope, $element, $attrs) {
-            $scope.title = $scope.year.name;
-
-            dataFactory.monthsForYear($scope.year.name, function (months) {
-                $scope.months = months;
-            });
-        }
-    }
-}]);
-
-app.controller('AppController', ['dataFactory', function (dataFactory) {
-    var self = this;
-
-    dataFactory.years(function (years) {
-        self.years = years;
-    });
-
-    dataFactory.latest(function (year, month, day) {
-        self.latest = {
-            'year': year,
-            'month': month,
-            'day': day,
-            'title': moment(year + "-" + month + "-" + day, "YYYY-MM-DD hh:mm:ss").format("LL")
-        };
-    });
-}]);
-
-app.controller('ShowController', ['$routeParams', 'dataFactory', function ($routeParams, dataFactory) {
-    var self = this;
-
-    self.title = moment($routeParams.year + "-" + $routeParams.month + "-" + $routeParams.day, "YYYY-MM-DD hh:mm:ss").format("LL");
-
-    self.year = $routeParams.year;
-    self.month = $routeParams.month;
-    self.day = $routeParams.day;
-
-    self.slideInterval = 3000;
-
-    dataFactory.filesForYearMonthDay($routeParams.year, $routeParams.month, $routeParams.day, function (files) {
-        self.files = files;
-    });
-
-    self.setSpeed = function(speed) {
-        self.slideInterval = speed;
-    }
-}]);
-
