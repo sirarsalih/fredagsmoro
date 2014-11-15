2936f45dd727ae7327377b90ccc4da92bd131bc4
Chris Searle <chris@chrissearle.org>
2014-11-15 14:32:04 +0100

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,6 +72,9 @@ module.exports = function (grunt) {
                     message: 'Test'
                 }
             }
+        },
+        gitpush: {
+            newweek: {}
         }
     });
 
