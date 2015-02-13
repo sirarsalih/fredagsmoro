6c3ad5ecfe0529b3efc1394f8a5c3457cbc75c2e
Chris Searle <chris@chrissearle.org>
2015-02-13 14:26:26 +0100

    New Week

--- Gruntfile.js
+++ Gruntfile.js
@@ -81,7 +81,7 @@ module.exports = function (grunt) {
         gitpush: {
             newweek: {
                 options: {
-                    remote: "github"
+                    remote: "origin"
                 }
             }
         }
