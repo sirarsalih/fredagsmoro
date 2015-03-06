d04c02942e897884e4d13e1ec1689d833e5f3d86
Chris Searle <chris@chrissearle.org>
2015-03-06 12:51:00 +0100

    Max 700

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -3,9 +3,9 @@
 for F in `find ./content -name *.jpg -print`; do
   width=`identify -format %w $F`
   echo Testing $F with width $width
-  if [ $width -gt 1000 ]; then
+  if [ $width -gt 700 ]; then
     echo $F Too big
-    convert $F -resize 1000x1000\> $F.new
+    convert $F -resize 700x700\> $F.new
     rm $F
     mv $F.new $F
   fi
